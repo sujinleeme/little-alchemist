042000c645f031e53040ff3ee59c0c3f7c09ee94
sujinleeme <sujinlee.me@gmail.com>
2021-09-29 17:02:35 +0200

    chapter5: submit exercise: functions-1

---
+++ programming-elixir-1.6/exercise/functions-1.exs
@@ -0,0 +1,23 @@
+# Exercise: Anonymous Functions
+
+# Go into IEx. Create and run the functions that do the following:
+# - list_concat.([:a, :b], [:c, :d]) #=> [:a, :b, :c, :d]
+# - sum.(1, 2, 3) #=> 6
+# - pair_tuple_to_list.( { 1234, 5678 } ) #=> [ 1234, 5678 ]
+
+# Answers
+# list_concat = fn a, b -> a ++ b end
+list_concat = &(&1 ++ &2)
+
+# # sum = fn a, b, c -> a + b + c end
+sum = &(&1 + &2 + &3)
+
+# pair_tuple_to_list = fn tuple -> Tuple.to_list(tuple) end
+# # pair_tuple_to_list = &(Tuple.to_list(&1))
+# # pair_tuple_to_list = &Tuple.to_list(&1)
+pair_tuple_to_list = &Tuple.to_list/1
+
+# Prints
+IO.inspect list_concat.([:a, :b], [:c, :d]), label: "Func: list_concat"
+IO.inspect sum.(1, 2, 3), label: "Func: sum"
+IO.inspect pair_tuple_to_list.({1234, 5678}), label: "Func: pair_tuple_to_list"
