cd917af1eafcbcf5a893f4fbb43997a9152dc54e
sujinleeme <sujinlee.me@gmail.com>
2021-10-05 15:39:54 +0200

    fix pair_tuple_to_list func

--- programming-elixir-1.6/exercise/functions-1.exs
+++ programming-elixir-1.6/exercise/functions-1.exs
@@ -9,15 +9,18 @@
 # list_concat = fn a, b -> a ++ b end
 list_concat = &(&1 ++ &2)
 
-# # sum = fn a, b, c -> a + b + c end
+# sum = fn a, b, c -> a + b + c end
 sum = &(&1 + &2 + &3)
 
 # pair_tuple_to_list = fn tuple -> Tuple.to_list(tuple) end
-# # pair_tuple_to_list = &(Tuple.to_list(&1))
-# # pair_tuple_to_list = &Tuple.to_list(&1)
-pair_tuple_to_list = &Tuple.to_list/1
+# pair_tuple_to_list = &(Tuple.to_list(&1))
+# pair_tuple_to_list = &Tuple.to_list(&1)
+# pair_tuple_to_list = &Tuple.to_list/1
 
-# Prints
+# pair_tuple_to_list = fn {a, b} -> [a, b] end
+pair_tuple_to_list = &([elem(&1, 0), elem(&1, 1)])
+
+# Print outputs
 IO.inspect list_concat.([:a, :b], [:c, :d]), label: "Func: list_concat"
 IO.inspect sum.(1, 2, 3), label: "Func: sum"
 IO.inspect pair_tuple_to_list.({1234, 5678}), label: "Func: pair_tuple_to_list"
