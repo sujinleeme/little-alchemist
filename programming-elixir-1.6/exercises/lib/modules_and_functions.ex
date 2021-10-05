5b0fc3617b725c5bfade9f36fcb930be9a747347
sujinleeme <sujinlee.me@gmail.com>
2021-10-05 19:33:01 +0200

    submit ch6-functions-1 exercise

---
+++ programming-elixir-1.6/exercises/lib/modules_and_functions.ex
@@ -0,0 +1,13 @@
+# Exercise: Modules and Functions-1 (Page 55)
+# Chapter 6: Modules and Named Functions
+
+# Exercise: Modules and Functions-1 (Page 55)
+# - Extend the Times module with a triple function, that multiplies its parameter by three
+# - Run the result in iex. Use both techniques to compile the file.
+# - Add a quadruple function. (Maybe it could call the double function….)
+
+defmodule Times do
+  def double(n), do: n * 2
+  def triple(n), do: n * 3
+  def quardruple(n), do: double(double(n))
+end
