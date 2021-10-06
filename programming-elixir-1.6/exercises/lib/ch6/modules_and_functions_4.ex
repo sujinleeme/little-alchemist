8fc6353ffc8a090126fac90286c781dd60a61d54
sujinleeme <sujinlee.me@gmail.com>
2021-10-06 12:21:17 +0200

    submit ch6-functions-4 exercise

---
+++ programming-elixir-1.6/exercises/lib/ch6/modules_and_functions_4.ex
@@ -0,0 +1,18 @@
+# Chapter 6: Modules and Named Functions
+
+# Exercise: Exercise: Modules and Functions-4 (Page 57)
+# Implement and run a function sum(n) that uses recursion to calculate the sum of the integers from 1 to n . You’ll need to write this function inside a module in a separate file. Then load up iex, compile that file, and try your function.
+
+# Exercise: Modules and Functions-5 (Page 57)
+# Write a function gcd(x,y) that finds the greatest common divisor between two
+# nonnegative integers. Algebraically, gcd(x,y) is x if y is zero, gcd(y,
+# rem(x,y)) otherwise.
+
+# Answer
+defmodule Math do
+  def sum(1), do: 1
+  def sum(n), do: n + sum(n-1)
+
+  def gcd(x, 0), do: x
+  def gcd(x, y), do: gcd(y, rem(x, y))
+end
