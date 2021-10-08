7f136b95e082dd3e3cb5cd3a77e36a1f7f28e52d
sujinleeme <sujinlee.me@gmail.com>
2021-10-08 20:13:26 +0200

    clean

--- programming-elixir-1.6/exercises/lib/ch5/functions_4.exs
+++ programming-elixir-1.6/exercises/lib/ch5/functions_4.exs
@@ -1,5 +1,7 @@
+# -------
+# Chapter 5: Anonymous Functions
+#
 # Exercise: Functions-4
-
 # Write a function prefix that takes a string.
 # It should return a new function that takes a second string.
 # When that second function is called, it will return a string containing the first string, a space, and the second string.
@@ -9,8 +11,14 @@
 # "Mrs Smith"
 # iex> prefix.("Elixir").("Rocks")
 # "​Elixir Rocks"​”
+# -------
+
+# -------
+# Answers
 
 prefix = fn pre -> fn word -> "#{pre} #{word}" end end
 mrs = prefix.("Mrs")
 IO.inspect mrs.("Smith")
 IO.inspect prefix.("Elixir").("Rocks")
+
+# -------
