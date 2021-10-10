3a59565ba5c52745aed42a7b8eb8267f6ac0be1f
sujinleeme <sujinlee.me@gmail.com>
2021-10-10 16:59:24 +0200

    remove wrapping func

--- programming-elixir-1.6/exercises/lib/ch6/modules_and_functions_7.ex
+++ programming-elixir-1.6/exercises/lib/ch6/modules_and_functions_7.ex
@@ -11,32 +11,29 @@
 # - Return the current working directory of the process. (Elixir)
 # - Convert a string containing JSON into Elixir data structures. (Just find, #don’t install)
 # - Execute an command in your operating system’s shell
-
 # --------
 
-# --------
-# Answers
-#
 # - Convert a float to a string with 2 decimal digits. (Erlang)
-float_to_string = &(:erlang.float_to_list(&1, [{:decimals, 2}]))
-IO.inspect float_to_string.(2.123123)
+float = 2.123123
+float_to_string = :erlang.float_to_list(float, [{:decimals, 2}])
+IO.inspect float_to_string
 
+# Get the value of an operating system environment variable. (Elixir)
+dir = "HOME"
+env_path = System.get_env(HOME)
+IO.inspect env_path
 
-# - Get the value of an operating system environment variable. (Elixir)
-get_env_path = &(System.get_env(&1))
-IO.inspect get_env_path("HOME")
+# Return the extension component of a file name (so return .exs if given "dave/test.exs" (Elixir)
+file_name = "test.exs"
+extension = Path.extname(file_name)
+IO.inspect extension
 
-# - Return the extension component of a file name (so return .exs if given "dave/test.exs" (Elixir)
-get_extension = &(Path.extname(&1))
-IO.inspect get_extension("HOME")
-
-# - Return the current working directory of the process. (Elixir)
+# Return the current working directory of the process. (Elixir)
 {:ok, path} = File.cwd
+IO.inspect path
 
-# - Convert a string containing JSON into Elixir data structures. (Just find, #don’t install)
+# Convert a string containing JSON into Elixir data structures. (Just find, #don’t install)
 # Poison: https://github.com/devinus/poison
 
-# - Execute an command in your operating system’s shell
+# Execute an command in your operating system’s shell
 System.cmd("ls", ["-al"])
-
-# --------
